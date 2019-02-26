bfc45515b40fb08dfd669ed15bdd07787d8da839
Olalekan Ilori <47522482+Parakconcepts@users.noreply.github.com>
2019-02-26 16:26:44 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Laptop:
+    def __init__(self, name, size, cpu, ram, is_new):
+        self.name = name
+        self.size = size
+        self.cpu  = cpu
+        self.ram  = ram
+        self.is_new = is_new
+
+class Quiz:
+    def __init__(self, prompt,answer):
+        self.prompt = prompt
+        self.answer = answer
+
